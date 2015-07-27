8fcb7d456594dd43bc192c554ce31ce5896ab913
AlexKir <kirillov.alex@gmail.com>
2015-07-27 21:46:37 +0300

    yar

--- get_scripts/uniyar.pl
+++ get_scripts/uniyar.pl
@@ -33,30 +33,37 @@ $d = strftime "%d_%m", localtime;
 my $vuz = 'uniyar';
 my $spec = '';
 my $fio = '';
-$fname = $dname.'/'.$vuz.'.pdf';
 
+sub spec {
+
+my $url = shift;
+my $spec = shift;
 my $incomplete = 0;
 
+$fname = $dname.'/'.$vuz.'-'.$spec.'.pdf';
 getstore($url, $fname);
-my $cmd = "bash pdf2text.sh ".$dname."/uniyar.pdf ".$dname."/uniyar.txt";
+my $cmd = "bash pdf2text.sh ".$fname." ".$dname."/uniyar.txt";
 system($cmd);
-
+#die;
 open(FILE,$dname."/uniyar.txt");
 while(my $s = <FILE>)
 	{
-    # 33701        ���������� ����� ���������� ��������� �������� �      05.03.06 �������� �            �����            ���������         ���           1
     #           1                 2
     #print $s."\n";
     #print $s;
 	$s = trim(decode('utf8',$s));
-    if ( $s =~ /\d+\s+(\S+ \S+ \S+).*(\S\S\.\S+\.\S+).*1$/ ) {
+    #print "s1 = ".$s."\n";
+    # 1                33515 Пуханов Вячеслав Сергеевич         мат - 86; рус - 87; инф - 97;   (сочинение);                274 Нет
+    #301   35904 Басков Михаил Вадимович       мат - 50; рус - 66; инф - 44;   (сочинение)              162 Нет
+    if ( $s =~ /(\d+)\s+\d+\s+(\S+ \S+ \S+).*\s+(\d+)\s+(\S+)$/ ) {
+      #print $s."\n";
 			print $vuz.";";
-			print trim($2).";";
-			print trim('0').";";  # �
-			print trim($1).";";  # ���
-			print trim('0').";";    # ���������
-			print trim('n/a').";"; 	# ��������
-			print trim('0');	   	# ����
+			print $spec.";";
+			print trim($1).";";  #
+			print trim($2).";";      #  ФИО
+			print trim('0').";";     # Приоритет
+			print trim($3).";"; 	 # Оригинал
+			print trim($4);	   	   #  Балл
 			print "\n";
       $incomplete = 0;
 		}
@@ -70,14 +77,23 @@ while(my $s = <FILE>)
           $fio = $fio." ".$1;
           print $vuz.";";
     			print $spec.";";
-    			print trim('0').";";  # �
-    			print trim($fio).";";  # ���
-    			print trim('0').";";    # ���������
-    			print trim('n/a').";"; 	# ��������
-    			print trim('0');	   	# ����
+    			print trim('0').";";  #
+    			print trim($fio).";";  #
+          print trim('0').";";    #
+    			print trim('n/a').";"; 	#
+    			print trim('0');	   	#
     			print "\n";
           $incomplete = 0;
       }
     } # else
 	} # while
 close(FILE);
+}
+
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/mkn.pdf','02.03.01');
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/pmi_m.pdf','01.03.02-M');
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/pmi_ivt.pdf','01.03.02-IVT');
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/fiit.pdf','02.03.02');
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/kb.pdf','10.05.01');
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/pie.pdf','09.03.03');
+spec ('http://priem.uniyar.ac.ru/entrant2015/rati/ikt.pdf','11.03.02');
